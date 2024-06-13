b8a3e84c846374a386c83e69d58841c32f20f413
MadhuMishra663 <madhumishra663@gmail.com>
2024-06-13 21:43:08 +0530

    Admin2

---
+++ Frontend/src/components/Admin/Adminmenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Adminmenu = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Adminmenu
--- Frontend/src/components/Admin/Navbar.jsx
+++ Frontend/src/components/Admin/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Admin = () => {
+const Navbar = () => {
   return (
     <div>
-       
+      
     </div>
   )
 }
 
-export default Admin
+export default Navbar
